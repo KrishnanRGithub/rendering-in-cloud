b7c7e8f561183aaee1110cac57a8035175da4030
Krishnan R <vinokrish001@gmail.com>
2022-12-20 03:37:09 +0530

    updated VR application URL

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,6 +1,6 @@
 import React, { useCallback, useState } from "react";
 import Modal from "./Modal";
-const VRAPP_URL="https://google.com"
+const VRAPP_URL="https://vr-fyp.pages.dev/"
 const Home = () => {
   console.log("Launching the UI thing");
   const url = VRAPP_URL;
